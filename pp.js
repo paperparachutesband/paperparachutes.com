733700bc197122989e3f2aaad57c48d1c62c0901
Marissa Beatty <MarissaBeatty@users.noreply.github.com>
2018-10-30 19:57:48 -0400

    updated css

--- pp.js
+++ pp.js
@@ -6,27 +6,20 @@
   var bioModal = document.querySelector('#bioModal');
   var bioIcon = document.querySelector('#bioIcon');
   var bioButton = document.querySelector('#bioButton');
-
   var contactButton = document.querySelector('#contactButton');
   var contactModal = document.querySelector('#contactModal');
   var backToTop = document.querySelector('#top');
 
   // SEND USER TO BAND BIOGRAPHY ON CLICK
   bioIcon.addEventListener('click', function(event) {
+    event.preventDefault();
     bio.scrollIntoView({behavior: "smooth"});
-      });
+  });
 
   // BACK TO TOP
   backToTop.addEventListener('click', function(event) {
+    event.preventDefault();
     grid.scrollIntoView({behavior: "smooth"});
-    
-    // (function smoothscroll(){
-    //     var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    //     if (currentScroll > 0) {
-    //          window.requestAnimationFrame(smoothscroll);
-    //          window.scrollTo (0,currentScroll - (currentScroll/5));
-    //     }
-    // })();
   });
 
   // OPEN BAND BIOGRAPHY MODAL
@@ -43,8 +36,6 @@
   });
 
   // CLOSE MODALS
-
-
   document.addEventListener('click', function (event) {
       if ( event.target.classList.contains( 'close' ) ) {
         bioModal.classList.remove('visible');
